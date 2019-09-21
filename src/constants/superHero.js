61faa0b9847e077ebcd299e17fcbe2faa17b3275
ashudeshwal999 <ashudeshwal999@gmail.com>
2019-09-22 02:30:37 +0530

    --update : add heros constants

---
+++ src/constants/superHero.js
@@ -0,0 +1,24 @@
+const phoneMapping = {
+  1: [],
+  2: ['a', 'b', 'c'],
+  3: ['d', 'e', 'f'],
+  4: ['g', 'h', 'i'],
+  5: ['j', 'k', 'l'],
+  6: ['m', 'n', 'o'],
+  7: ['p', 'q', 'r', 's'],
+  8: ['t', 'u', 'v'],
+  9: ['w', 'x', 'y', 'z'],
+  0: [],
+  '*': [],
+  '#': [],
+};
+const heros = [
+  'SUPERMAN', 'THOR', 'ROBIN', 'IRONMAN', 'GHOSTRIDER', 'CAPTAINAMERICA', 'FLASH', 'WOLVERINE',
+  'BATMAN', 'HULK', 'BLADE', 'PHANTOM', 'SPIDERMAN', 'BLACKWIDOW', 'HELLBOY', 'PUNISHER',
+  'ADGJ', 'BEHK'
+];
+
+module.exports = {
+  heros,
+  phoneMapping,
+};
